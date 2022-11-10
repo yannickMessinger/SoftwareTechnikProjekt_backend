b3c6e5b6febcd5130e33e8e2a217f3493e9db45f
Christian Zernickel <christian.zernickel@student.hs-rm.de>
2022-11-10 23:29:18 +0100

    init: project

---
+++ src/main/java/de/hsrm/mi/swt02/backend/swagger/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package de.hsrm.mi.swt02.backend.swagger;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+// https://springdoc.org/
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("backend-api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
---
+++ src/test/java/de/hsrm/mi/swt02/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.swt02.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
