ab196853859733158df666f0ed39b0541acef4a8
Christian Zernickel <christian.zernickel@student.hs-rm.de>
2022-11-16 19:50:24 +0100

    feat: added OpenAPI documentation

--- src/main/java/de/hsrm/mi/swt02/backend/api/lobby/LobbyRestController.java
+++ src/main/java/de/hsrm/mi/swt02/backend/api/lobby/LobbyRestController.java
@@ -1,71 +1,87 @@
 package de.hsrm.mi.swt02.backend.api.lobby;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import de.hsrm.mi.swt02.backend.api.lobby.dtos.GetLobbyResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import de.hsrm.mi.swt02.backend.api.lobby.dtos.AddLobbyRequestDTO;
-import de.hsrm.mi.swt02.backend.api.lobby.dtos.GetLobbyResponseDTO;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/lobby")
 public class LobbyRestController {
-    
-    @Autowired 
+
+    @Autowired
     private LobbyServiceImpl lobbyService;
 
-
-   
-
-     @GetMapping("")
-        public ResponseEntity<List<GetLobbyResponseDTO>> getAllLobbys() {
-        List<GetLobbyResponseDTO> lobbyDTOs = new ArrayList<GetLobbyResponseDTO>(
-            lobbyService.findAllLobbys()
+    @Operation(summary = "Get all available Lobby´s")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found Lobby´s")})
+    @GetMapping("")
+    public ResponseEntity<List<GetLobbyResponseDTO>> getAllLobbys() {
+        List<GetLobbyResponseDTO> lobbyDTOs = new ArrayList<>(
+                lobbyService
+                        .findAllLobbys()
                         .stream()
-                       .map(GetLobbyResponseDTO::from)
-                       .toList());
-            return new ResponseEntity <>(lobbyDTOs, HttpStatus.OK);
-     }
+                        .map(GetLobbyResponseDTO::from)
+                        .toList());
+        return new ResponseEntity<>(lobbyDTOs, HttpStatus.OK);
+    }
 
-
+    @Operation(summary = "Get Lobby by Lobby ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found Lobby")})
     @GetMapping("/{id}")
-    public ResponseEntity<GetLobbyResponseDTO> getSingleLobby(@PathVariable("id") long id){
+    public ResponseEntity<GetLobbyResponseDTO> getLobby(
+            @Schema(description = "Lobby ID", defaultValue = "1", required = true)
+            @PathVariable("id") long id) {
         GetLobbyResponseDTO lobby = GetLobbyResponseDTO.from(lobbyService.findLobbyById(id));
         return new ResponseEntity<>(lobby, HttpStatus.OK);
     }
 
+    @Operation(summary = "Post new Lobby")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Posted Lobby successfully")})
     @PostMapping("")
-    public ResponseEntity<Long> postNewLobby(@RequestBody Lobby lobby){
+    public ResponseEntity<Long> postNewLobby(
+            @Schema(description = "Lobby Dto",
+                    defaultValue =  "{\n" +
+                                    "  \"lobbyName\": \"Default Lobby\",\n" +
+                                    "  \"numOfPlayers\": \"1\",\n" +
+                                    "  \"lobbyState\": \"BUILD_MODE\"\n" +
+                                    "}",
+                    required = true)
+            @RequestBody Lobby lobby) {
 
         return new ResponseEntity<>(lobbyService.createLobby(lobby.getLobbyName()), HttpStatus.OK);
-
     }
 
-
+    @Operation(description = "Delete Lobby by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted Lobby")})
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteLobby(@PathVariable("id") long id){
+    public ResponseEntity<HttpStatus> deleteLobby(
+            @Schema(description = "Lobby ID", defaultValue = "1", required = true)
+            @PathVariable("id") long id) {
         lobbyService.deleteLobby(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     //Todo: match DTO's with DTO's from frontend!
+    @Operation(description = "Update Lobby with given JSON")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated Lobby")})
     @PutMapping("/{id}")
     public void updateLobby(
+            @Schema(description = "Lobby ID", defaultValue = "1", required = true)
+            @PathVariable("id") long id) {
 
-        @PathVariable("id") long id) {
-        
     }
-    
+
 }
