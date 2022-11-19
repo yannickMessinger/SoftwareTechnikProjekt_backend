cab61f0549d21b8dda6e64b81be9cd6a3b57a3dd
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-11-19 11:00:19 +0100

    init commit to start save functionality for Streetgrids coming from Frontend

---
+++ src/main/java/de/hsrm/mi/swt02/backend/api/streetgrid/StreetGrid.java
@@ -0,0 +1,5 @@
+package de.hsrm.mi.swt02.backend.api.streetgrid;
+
+public class StreetGrid {
+    
+}
---
+++ src/main/java/de/hsrm/mi/swt02/backend/api/streetgrid/StreetgridRestController.java
@@ -0,0 +1,25 @@
+package de.hsrm.mi.swt02.backend.api.streetgrid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("api/streetgrid")
+public class StreetgridRestController {
+    
+
+    @PostMapping("")
+    public  ResponseEntity<HttpStatus> postNewStreetGrid(@RequestBody StreetGrid streetGrid){
+        //ma schauen was vom frontend ankimmt
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+
+
+
+}
