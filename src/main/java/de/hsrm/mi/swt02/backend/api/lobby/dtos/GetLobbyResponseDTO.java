f8b282f33a49f6d749f4f6a8c0a09b4ea5334a86
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2022-11-29 23:50:16 +0100

    changed DTO for test purposes with frontend

--- src/main/java/de/hsrm/mi/swt02/backend/api/lobby/dtos/GetLobbyResponseDTO.java
+++ src/main/java/de/hsrm/mi/swt02/backend/api/lobby/dtos/GetLobbyResponseDTO.java
@@ -3,14 +3,15 @@ package de.hsrm.mi.swt02.backend.api.lobby.dtos;
 import de.hsrm.mi.swt02.backend.api.lobby.Lobby;
 import de.hsrm.mi.swt02.backend.api.lobby.LobbyMode;
 
-public record GetLobbyResponseDTO(String lobbyName, int numOfPlayers, LobbyMode lobbyMode){
+public record GetLobbyResponseDTO(String lobbyName, int numOfPlayers, LobbyMode lobbyMode, String hostName){
     
 
     public static GetLobbyResponseDTO from (Lobby lobby) {
         return new GetLobbyResponseDTO(
             lobby.getLobbyName(),
             lobby.getNumOfPlayers(),
-            lobby.getLobbyMode()
+            lobby.getLobbyMode(),
+            lobby.getHost().getUserName()
              
         );
     }
