9ea6297b8ac52869d748f8bd2aa70c45607de20f
Dennie Walo <dennie.walo@student.hs-rm.de>
2022-12-07 14:24:58 +0100

    changed query for userlogin

--- src/main/java/de/hsrm/mi/swt02/backend/api/userlogin/repository/UserloginRepository.java
+++ src/main/java/de/hsrm/mi/swt02/backend/api/userlogin/repository/UserloginRepository.java
@@ -4,11 +4,10 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import de.hsrm.mi.swt02.backend.domain.userlogin.Userlogin;
 
 public interface UserloginRepository extends JpaRepository<Userlogin, Long> {
-    @Query(value="SELECT * FROM USERLOGIN WHERE USERNAME = :username AND PASSWORD = :password", nativeQuery = true )
-    public Optional<Userlogin> findUserByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+    @Query("select u from Userlogin u where u.username=?1 and u.password=?2")
+    public Optional<Userlogin> findUserByUsernameAndPassword(String username, String password);
 }
