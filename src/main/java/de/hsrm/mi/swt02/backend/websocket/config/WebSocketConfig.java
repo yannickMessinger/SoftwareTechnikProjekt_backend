1a0da7979733aea6b81dd78cfe17ac1e392dc50d
Jona Simonavicius <jona.simonavicius@student.hs-rm.de>
2022-12-13 14:38:36 +0100

    fix: frontend couldn't connect with stompbroker

--- src/main/java/de/hsrm/mi/swt02/backend/websocket/config/WebSocketConfig.java
+++ src/main/java/de/hsrm/mi/swt02/backend/websocket/config/WebSocketConfig.java
@@ -9,15 +9,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stomp").withSockJS();
-    }
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp").setAllowedOriginPatterns("*");
     }
 }
