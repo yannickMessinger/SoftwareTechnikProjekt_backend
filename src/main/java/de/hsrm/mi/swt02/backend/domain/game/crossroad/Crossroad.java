ade5534679b2647fd305088eb11a4b8af8a2d491
Norbert Balaz <norbert.balaz@student.hs-rm.de>
2023-01-18 00:12:24 +0100

    Update: id generation via uuid

--- src/main/java/de/hsrm/mi/swt02/backend/domain/game/crossroad/Crossroad.java
+++ src/main/java/de/hsrm/mi/swt02/backend/domain/game/crossroad/Crossroad.java
@@ -2,9 +2,7 @@ package de.hsrm.mi.swt02.backend.domain.game.crossroad;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
@@ -24,20 +22,22 @@ import lombok.Setter;
 @Getter
 @Component
 public class Crossroad {
-    @Id
+    /* @Id
     @GeneratedValue
-    private long id;
+    private long id; */
+    private String id;
     private List<TrafficLight> trafficLights = new ArrayList<>();
     private boolean tlArrangement = false;
 
     public Crossroad() {
+        this.id = UUID.randomUUID().toString().replace("-", "");
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((trafficLights == null) ? 0 : trafficLights.hashCode());
         result = prime * result + (tlArrangement ? 1231 : 1237);
         return result;
@@ -52,7 +52,10 @@ public class Crossroad {
         if (getClass() != obj.getClass())
             return false;
         Crossroad other = (Crossroad) obj;
-        if (id != other.id)
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
             return false;
         if (trafficLights == null) {
             if (other.trafficLights != null)
