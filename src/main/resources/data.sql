46a565b0b3e60bf7d1ef9eb065e1e446f3e45424
Marc Bachmann <marc.bachmann@student.hs-rm.de>
2023-01-20 22:34:16 +0100

    added track and crossing model /upd  prev. picture

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,12 @@
 INSERT INTO MAP_OBJECT_TYPE (OBJECT_TYPE_ID, GROUP_ID, MODEL3D, NAME, ROTATION, TEXTURE, TYPE, VERSION) VALUES
-(0, 0, '/../../../src/assets/3D_Models/Streets/straight_road.gltf', 'Gerade', 0, '/img/streetplaner/object-icons/Road_straight.svg', 'STREET', 0),
-(1, 0, '/../../../src/assets/3D_Models/Streets/curved_road.gltf', 'Kurve', 0, '/img/streetplaner/object-icons/Road_curve.svg', 'STREET', 0),
-(2, 0, '/../../../src/assets/3D_Models/Streets/intersection_road.gltf', 'Kreuzung', 0, '/img/streetplaner/object-icons/Road_cross.svg', 'STREET', 0),
+(0, 0, '/../../../src/assets/3D_Models/Streets/straight_road.gltf', 'Gerade', 0, '/img/streetplaner/object-icons/gerade.png', 'STREET', 0),
+(1, 0, '/../../../src/assets/3D_Models/Streets/curved_road.gltf', 'Kurve', 0, '/img/streetplaner/object-icons/kurve.png', 'STREET', 0),
+(2, 0, '/../../../src/assets/3D_Models/Streets/intersection_road.gltf', 'Kreuzung', 0, '/img/streetplaner/object-icons/kreuzung.png', 'STREET', 0),
+
+(8, 0, '/../../../src/assets/3D_Models/Streets/streetCrossing.gltf', 'Fußgängerüberweg', 0, '/img/streetplaner/object-icons/crosswalk.png', 'STREET', 0),
+(9, 0, '/../../../src/assets/3D_Models/Railroad/straight_rail.gltf', 'Schiene Gerade', 0, '/img/streetplaner/object-icons/gerade_schiene.png', 'STREET', 0),
+(10, 0, '/../../../src/assets/3D_Models/Railroad/curved_rail.gltf', 'Schiene Kurve', 0, '/img/streetplaner/object-icons/curve_schiene.png', 'STREET', 0),
+
 (3, 1, '/../../../src/assets/3D_Models/Building/house_high.gltf', 'Großes Gebäude', 0, '/img/streetplaner/building/BigBuilding.png', 'BUILDING', 0),
 (4, 1, '/../../../src/assets/3D_Models/Building/house.gltf', 'Gebäude', 0, '/img/streetplaner/building/Building.png', 'BUILDING', 0),
 (5, 1, '/../../../src/assets/3D_Models/Building/shop.gltf', 'Supermarkt', 0, '/img/streetplaner/building/Supermarkt.png', 'BUILDING', 0),
