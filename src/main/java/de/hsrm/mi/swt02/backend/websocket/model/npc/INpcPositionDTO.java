cc683deb63e40b2066a801b5ca742d08ea01bc78
Yannick Messinger <yannick.messinger@student.hs-rm.de>
2023-01-28 11:05:49 +0100

    added viewRotation to be transferred in position message

--- src/main/java/de/hsrm/mi/swt02/backend/websocket/model/npc/INpcPositionDTO.java
+++ src/main/java/de/hsrm/mi/swt02/backend/websocket/model/npc/INpcPositionDTO.java
@@ -1,6 +1,6 @@
 package de.hsrm.mi.swt02.backend.websocket.model.npc;
 
-public record INpcPositionDTO(long npcId, double npcPosX, double npcPosZ, int npcRotation) {
+public record INpcPositionDTO(long npcId, double npcPosX, double npcPosZ, int npcRotation, double npcViewRotation) {
     
 }
 
