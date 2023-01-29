ce97de76bb2c71e33078a697215074951196ec13
Christian Zernickel <christian.zernickel@student.hs-rm.de>
2023-01-29 14:54:11 +0100

    chore: remove logger assertions

--- src/main/java/de/hsrm/mi/swt02/backend/api/game/crossroad/service/CrossroadServiceImpl.java
+++ src/main/java/de/hsrm/mi/swt02/backend/api/game/crossroad/service/CrossroadServiceImpl.java
@@ -1,23 +1,20 @@
 package de.hsrm.mi.swt02.backend.api.game.crossroad.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Service;
-
 import de.hsrm.mi.swt02.backend.api.game.crossroad.repository.CrossroadRepository;
 import de.hsrm.mi.swt02.backend.api.game.trafficLight.service.TrafficLightService;
 import de.hsrm.mi.swt02.backend.domain.game.crossroad.Crossroad;
 import de.hsrm.mi.swt02.backend.domain.game.trafficLight.Light;
 import de.hsrm.mi.swt02.backend.domain.game.trafficLight.TrafficLight;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.transaction.Transactional;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * The implementation of the {@link CrossroadService} interface. This service
@@ -39,9 +36,10 @@ public class CrossroadServiceImpl implements CrossroadService {
     /**
      * Constructor for creating an instance of the CrossroadServiceImpl.
      */
-    public CrossroadServiceImpl() {}
+    public CrossroadServiceImpl () {
+    }
 
-    public Crossroad createCrossroad() {
+    public Crossroad createCrossroad () {
         Crossroad crossroad = crRepo.save(new Crossroad());
         return crossroad;
     }
@@ -49,10 +47,9 @@ public class CrossroadServiceImpl implements CrossroadService {
     /**
      * This method starts the thread that handles the change of states of the
      * traffic lights at the crossroad.
-     * 
      */
     @Override
-    public void start(Long crId) {
+    public void start (Long crId) {
         Crossroad cr = crRepo.findById(crId).get();
         Thread thread = new Thread(() -> {
             changeStates(crId);
@@ -65,11 +62,11 @@ public class CrossroadServiceImpl implements CrossroadService {
     /**
      * This method stops the thread that handles the change of states of the traffic
      * lights at the crossroad.
-     * 
+     *
      * @return the thread that was interrupted.
      */
     @Override
-    public void stop(Long crId) {
+    public void stop (Long crId) {
         Thread thread = crTMap.get(crId);
         if (thread != null) {
             thread.interrupt();
@@ -79,14 +76,13 @@ public class CrossroadServiceImpl implements CrossroadService {
 
     /**
      * This method creates a number of traffic lights at the crossroad.
-     * 
+     *
      * @param numberOfTrafficLights the number of traffic lights to be created.
-     * 
      * @return a list of the created traffic light services.
      */
     @Override
     @Transactional
-    public List<TrafficLight> createTrafficLights(int numberOfTrafficLights, Long crId) {
+    public List<TrafficLight> createTrafficLights (int numberOfTrafficLights, Long crId) {
         Crossroad cr = crRepo.findById(crId).get();
         for (int i = 0; i < numberOfTrafficLights; i++) {
             Long tlId = tls.createTrafficLight().getId();
@@ -102,16 +98,14 @@ public class CrossroadServiceImpl implements CrossroadService {
 
     /**
      * This method changes the state of all traffic lights at the crossroad.
-     * 
      */
     @Override
-    public void changeStates(Long crId) {
+    public void changeStates (Long crId) {
         Crossroad cr = crRepo.findById(crId).get();
         boolean toggle = false;
         Map<Long, Light> tlMap = new HashMap<>();
 
         while (!crTMap.get(crId).isInterrupted()) {
-            logger.info("Bin drin");
             try {
                 if (!toggle) {
                     Thread.sleep(15000);
@@ -125,7 +119,6 @@ public class CrossroadServiceImpl implements CrossroadService {
                 }
                 messaging.convertAndSend("/topic/crossroad", tlMap);
             } catch (InterruptedException e) {
-                logger.info("Thread interrupted. Ending changeStates() method");
                 crTMap.get(crId).interrupt();
                 cr.setRunning(false);
                 break;
@@ -136,21 +129,21 @@ public class CrossroadServiceImpl implements CrossroadService {
     /**
      * This method returns the thread that handles the change of states of the
      * traffic lights at the crossroad.
-     * 
+     *
      * @return the thread that handles the change of states of the traffic lights.
      */
     @Override
-    public Thread getThread(Long crId) {
+    public Thread getThread (Long crId) {
         return crTMap.get(crId);
     }
 
     @Override
-    public Crossroad getCrossroad(Long crId){
+    public Crossroad getCrossroad (Long crId) {
         return crRepo.findById(crId).get();
     }
 
     @Override
-    public void deleteCrossroad(Long crId){
+    public void deleteCrossroad (Long crId) {
         stop(crId);
         crRepo.deleteById(crId);
     }
